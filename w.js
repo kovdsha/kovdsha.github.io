0c89991a1fe771aa2f8c1c38c8c8082fbf0605c4
kovdsha <115065584+kovdsha@users.noreply.github.com>
2023-10-26 22:05:40 +0300

    Delete w.js

--- w.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const menuNav = document.querySelector('.menu-nav');
-
-menuToggle.addEventListener('click', () => {
-  menuNav.classList.toggle('show-menu');
-});s
